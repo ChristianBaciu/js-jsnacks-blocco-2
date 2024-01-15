ec139fb78dd4b0a34ffecb57a08358c0ec90f72f
ChristianBaciu <christian3110@outlook.it>
2024-01-15 16:43:07 +0100

    snack 3

--- for 2/main.js
+++ for 2/main.js
@@ -1,11 +1,28 @@
-let primoArray = [1,2,3,4,5]
-let secondoArray = [1,2,3,4,5,6,7,8,9,10]
 const attivaHtml = document.getElementById("attiva")
 attivaHtml.addEventListener('click', function() {
-    
 
+    let primoArray = [1, 2, 3, 4, 5]
     
-    console.log(primoArray);
-    console.log(secondoArray);
+    let secondoArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    
+    let arrayMinore;
+
+    if(primoArray.lenght > secondoArray.lenght){
+        arrayMinore = array2
+    }else if(primoArray.lenght < secondoArray.lenght){
+        arrayMinore = primoArray
+    }
+    
+    function RandomNumber(min,max){
+        return Math.floor(Math.random()*(max-min + 1) + min)
+    }
+
+    for(let i = 0; primoArray.lenght !== secondoArray.lenght; i++){
+        let inserisciNumero = RandomNumber( 1,100 )
+
+        arrayMinore.push( inserisciNumero )
+    }
+
+    console.log(primoArray, secondoArray)
 
 })
